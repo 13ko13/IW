a5bb47f172792ead4fa10bf4cb903ac4ee8c9fc2
ko1307 <208754810+13ko13@users.noreply.github.com>
2025-07-27 13:28:19 +0900

    up

---
+++ SummerVacation_kadai/DrawPixel.cpp
@@ -0,0 +1,43 @@
+#include "DxLib.h"
+#include "Game.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(true);
+
+	SetGraphMode(Game::kScreenWidth, Game::kScreenHeight,0);
+
+	SetWindowSizeChangeEnableFlag(TRUE);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	//描画先を裏画面に設定
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (ProcessMessage() == 0)
+	{
+		//画面をクリア
+		ClearDrawScreen();
+
+		//ESCキーが押されたらウィンドウを閉じる
+		if (CheckHitKey(KEY_INPUT_ESCAPE))
+		{
+			break;
+		}
+
+		//ここにゲームの処理などを書く
+		
+
+		//裏画面の内容を表画面に反映
+		ScreenFlip();
+
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
