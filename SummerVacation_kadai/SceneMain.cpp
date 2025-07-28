589fef4b57ec0f5f92d82bec1bf03b981a5590f5
ko1307 <208754810+13ko13@users.noreply.github.com>
2025-07-28 21:05:20 +0900

    up

---
+++ SummerVacation_kadai/SceneMain.cpp
@@ -0,0 +1,25 @@
+#include "SceneMain.h"
+
+SceneMain::SceneMain()
+{
+}
+
+SceneMain::~SceneMain()
+{
+}
+
+void SceneMain::Init()
+{
+}
+
+void SceneMain::End()
+{
+}
+
+void SceneMain::Update()
+{
+}
+
+void SceneMain::Draw()
+{
+}
